6f6cdfc38999de90b55141a9c36154adbe77c6eb
khalil-hamidani <hamidani2002@gmail.com>
2023-06-23 03:11:35 +0100

    app.cpython-38.pyc app.py error.html

--- Source/app.py
+++ Source/app.py
@@ -92,7 +92,7 @@ def individual_report_form():
         description = request.form.get("message")
 
         # Check if any of the required fields are empty
-        if not (first_name or last_name or email or date or description):
+        if not (first_name and last_name and email and date and description):
             # Render the HTML template for the error page and return it
             return render_template("error.html")
 
@@ -170,7 +170,7 @@ def group_report_form():
         other = request.form.get("otherissue")
         description = request.form.get("message")
         
-        if not (first_name or last_name or email or date or description):
+        if not (first_name and last_name and email and date and description):
             # Render the HTML template for the error page and return it
             return render_template("error.html")
 
@@ -230,8 +230,7 @@ def government_report_form():
     if request.method == "GET":
         return render_template("government-report-form.html")
     else:
-        # first_name = request.form.get("first_name")
-        first_name = None
+        first_name = request.form.get("first_name")
         last_name = request.form.get("last_name")
         email = request.form.get("contact_email")
         telephone = request.form.get("telephone_number")
@@ -248,13 +247,12 @@ def government_report_form():
         country = request.form.get("country")
         email_address = request.form.get("email2")
         website = request.form.get("website")
-        print(website)
         date = request.form.get("Date")
         incident_type = request.form.get("type")
         other = request.form.get("otherissue")
         description = request.form.get("message")
         
-        if not (first_name or last_name or email or date or description):
+        if not (first_name and last_name and email and date and description):
             # Render the HTML template for the error page and return it
             return render_template("error.html")
 
